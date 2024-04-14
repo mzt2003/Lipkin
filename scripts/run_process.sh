be081470dcc87fd962be4b23dead47df08f89990
Zetian <2042934905@qq.com>
2024-04-15 02:44:54 +0800

    test

--- scripts/run_process.sh
+++ scripts/run_process.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "Compiling C++ program..."
-g++ ../src/main.cpp -o ../bin/data_generator
+echo "Configuring and building C++ program with CMake..."
+mkdir -p ../build && cd ../build  # 创建并进入构建目录
+cmake ..  # 生成 Makefile
+make      # 构建项目
 
 echo "Running C++ program..."
-../bin/data_generator
+../bin/data_generator  # 执行生成的可执行文件
 
 echo "Running MATLAB script..."
 matlab -batch "run('../matlab/plot_data.m')"
 
 echo "Done."
-chmod +x scripts/run_process.sh
+
